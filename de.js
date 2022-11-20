fdc476cb166b1f820001749a74da72bcef1a138d
NicheYoutubeShorts <117995043+NicheYoutubeShorts@users.noreply.github.com>
2022-11-20 14:33:53 +0800

    Update de.js

--- de.js
+++ de.js
@@ -20,7 +20,7 @@ function autoRelated(data) {
     	var kw = out[i];
 		var kata = document.write(spin(sample_str));
 		var kata1 = document.write(spin(sample_str1));
-        dom += `<aside><img alt="${kw}" src="https://tse1.mm.bing.net/th?q=${encodeURIComponent(kw)}" width="320" height="320" style="width:100%;height:auto;margin-right: 8px;margin-bottom: 8px;" /><small>title: <i>${kw}</i></small><h3><a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a></h3><p align="justify"><b>${kw}</b> ${kata} dengan ${postTitle}. ${kata2} lihat sebagai referensi untuk menentukan sesuai dengan kenginan. Oleh karena itu, jangan ragu untuk menyimpan data ${kw}</p> <p align="justify"> Jika suatu saat anda memerlukannya, akan memudahkan anda untuk mencarinya.</p></aside>`;
+        dom += `<aside><img alt="${kw}" src="https://tse1.mm.bing.net/th?q=${encodeURIComponent(kw)}" width="320" height="320" style="width:100%;height:auto;margin-right: 8px;margin-bottom: 8px;" /><small>title: <i>${kw}</i></small><h3><a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a></h3><p align="justify"><b>${kw}</b> ${kata} dengan ${postTitle}. ${kata1} lihat sebagai referensi untuk menentukan sesuai dengan kenginan. Oleh karena itu, jangan ragu untuk menyimpan data ${kw}</p> <p align="justify"> Jika suatu saat anda memerlukannya, akan memudahkan anda untuk mencarinya.</p></aside>`;
 		
 		
     }
