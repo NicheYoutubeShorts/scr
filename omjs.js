cb7ebe76c476a56acc4dc4243fbbd15650b6d7f8
NicheYoutubeShorts <117995043+NicheYoutubeShorts@users.noreply.github.com>
2022-11-20 13:42:10 +0800

    Update omjs.js

--- omjs.js
+++ omjs.js
@@ -17,7 +17,7 @@ function autoRelated(data) {
 		
     for (i = 0; i < out.length; i++) {
     	var kw = out[i];
-		var kata= document.write(spin(sample_str));
+		var kata = write(spin(sample_str));
         dom += `<aside><img alt="${kw}" src="https://tse1.mm.bing.net/th?q=${encodeURIComponent(kw)}" width="320" height="320" style="width:100%;height:auto;margin-right: 8px;margin-bottom: 8px;" /><small>title: <i>${kw}</i></small><h3><a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a></h3><p align="justify"><b>${kw}</b> ${kata}adalah hasil yang paling sesuai dengan ${postTitle}. Semua yang ada disini dapat anda lihat sebagai referensi untuk menentukan sesuai dengan kenginan. Oleh karena itu, jangan ragu untuk menyimpan data ${kw}</p> <p align="justify"> Jika suatu saat anda memerlukannya, akan memudahkan anda untuk mencarinya.</p></aside>`;
 		
 		
