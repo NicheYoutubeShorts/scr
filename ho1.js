9a6a51615ce0c0125c3ec464d88fab7923f9c4c8
NicheYoutubeShorts <117995043+NicheYoutubeShorts@users.noreply.github.com>
2022-11-21 00:03:47 +0800

    Update ho1.js

--- ho1.js
+++ ho1.js
@@ -1,5 +1,5 @@
 function autoRelated(data) {
-    	    
+   var out = data[1]; 	    
 	
 	var SPINTAX_PATTERN = /\{[^"\r\n\}]*\}/;
 	var spin = function (spun) {
@@ -14,12 +14,12 @@ function autoRelated(data) {
  var sample_str="{adalah|merupakan|pencarian} {yang paling sesuai|yang anda butuhkan}";
  var sample_str1="{Semua yang|Apa yang|Pencarian yang} {ente|anda|saudara|teman}";
  
-    for (i = 0; i < out.length; i++) {
-    	var kw = out[i];
+    
+    	var kw = out[0];
 		var kata = spin(sample_str);
 		var kata1 = spin(sample_str1);
         dom += `<b>${kw}</b> ${kata} dengan ${postTitle}. ${kata1} lihat sebagai referensi untuk menentukan sesuai dengan kenginan. Oleh karena itu, jangan ragu untuk menyimpan data ${kw}. Jika suatu saat anda memerlukannya, akan memudahkan anda untuk mencarinya.`;
 		
-	}
+	
     
 }
